d38a5e555320f277bd1aed38a2617b02a9d8048d
Saeed-Hussain <hussainsaeed724@gmail.com>
2025-07-21 22:22:05 +0500

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ var sidemenu = document.getElementById("sidemenu");
     setTimeout(function(){
       document.getElementById('loader').style.display = 'none';
       document.getElementById('content').style.display = 'block';
-    }, 3000);
+    }, 1500);
   });
 
 	
